21ab3f19d6a40c060b950ad0efab474b565d06aa
llacuna750 <llacuna750@gmail.com>
2026-03-07 03:31:46 +0800

    New modified Site 1 files

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -8,9 +8,14 @@ use Illuminate\Validation\ValidationException;
 use Laravel\Lumen\Exceptions\Handler as ExceptionHandler;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Throwable;
+use App\Traits\ApiResponser;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Http;
 
 class Handler extends ExceptionHandler
 {
+    use ApiResponser;
+
     /**
      * A list of the exception types that should not be reported.
      *
@@ -46,9 +51,44 @@ class Handler extends ExceptionHandler
      * @return \Illuminate\Http\Response|\Illuminate\Http\JsonResponse
      *
      * @throws \Throwable
-     */
+    */
+
+
     public function render($request, Throwable $exception)
     {
-        return parent::render($request, $exception);
+        // Handle HttpException  or  http not found 
+        if ($exception instanceof HttpException) {
+            $code = $exception->getStatusCode();
+            $message = Response::$statusTexts[$code];
+
+            return $this->errorResponse($message, $code);
+        }
+
+        // istance not found
+        if ($exception instanceof ModelNotFoundException) {
+            $model = strtolower(class_basename($exception->getModel()));
+
+            return $this->errorResponse("Does not exist any instance of {$model} with the specified id", Response::HTTP_NOT_FOUND);
+        }
+
+        // validation exception
+        if ($exception instanceof ValidationException) {
+            $errors = $exception->validator->errors()->getMessages();
+            return $this->errorResponse($errors, Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+
+        // access to forbidden
+        if ($exception instanceof AuthorizationException) {
+            return $this->errorResponse($exception->getMessage(), Response::HTTP_FORBIDDEN);
+        }
+
+        // if your are running in development environment,      you want to see the error details
+        if (env('APP_DEBUG', false)) {
+            return parent::render($request, $exception);
+        }
+
+        return $this->errorResponse('Unexpected Exception. Try later', Response::HTTP_INTERNAL_SERVER_ERROR);
+
     }
 }
